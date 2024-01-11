4ecfa5e6fdf8aaabec6827ebaae3e0d4501d1791
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2024-01-11 12:26:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,7 @@ def loopthread(message,otherss=False):
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([
-        [ InlineKeyboardButton("UPDATE CHANNEL", url="https://t.me/main_channel_bot_update")],
-        [ InlineKeyboardButton("MOVIE CHANNEL", url="https://t.me/Ott_Movie_Request_Group")],
+        [ InlineKeyboardButton("UPDATE CHANNEL", url="https://t.me/main_channel_bot_update")]
         [ InlineKeyboardButton("OWNER", url="https://telegram.me/SUDIPSAHA06") ]]), 
         reply_to_message_id=message.id)
 
